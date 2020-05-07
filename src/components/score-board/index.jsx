69c31290cf847020642132d576008d795917ac12
Nicolas Jacquet <nicolasjacquet89@gmail.com>
2020-05-07 20:42:09 +0200

    add score animation

--- src/components/score-board/index.jsx
+++ src/components/score-board/index.jsx
@@ -1,16 +1,21 @@
 // @flow
 import * as React from "react";
 import { useGame } from "./../../context/game-context";
+import Tada from "react-reveal/Tada";
 import css from "./style.module.scss";
 
 const ScoreBoard = (): React.Node => {
-    const { state } = useGame();
+    const {
+        state: { score },
+    } = useGame();
 
     return (
         <div className={css.container}>
             <p className={css.content}>
                 <span className={css.label}>SCORE</span>
-                <span className={css.score}>{state.score}</span>
+                <Tada spy={score}>
+                    <span className={css.score}>{score}</span>
+                </Tada>
             </p>
         </div>
     );
